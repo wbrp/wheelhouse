c3a66a6564e3e9ab5c720e418790190756a36ecc
Danilo Bargen <mail@dbrgn.ch>
2014-11-05 14:03:48 +0100

    Added oauth2client 1.3.2

---
+++ oauth2client.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export NAME=oauth2client
+export REPO=https://github.com/google/oauth2client
+export CWD=$(pwd)
+export PYTHON_BIN=/usr/bin/python2.7
+source $CWD/_pre.sh
+source $CWD/_post.sh
