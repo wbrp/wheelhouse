9465b1d9131236c3f082aaeb89c56031f773cc62
Danilo Bargen <mail@dbrgn.ch>
2014-11-05 14:05:38 +0100

    Added googleads 2.1.0

---
+++ googleads.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export NAME=googleads
+export REPO=https://github.com/googleads/googleads-python-lib
+export CWD=$(pwd)
+export PYTHON_BIN=/usr/bin/python2.7
+source $CWD/_pre.sh
+source $CWD/_post.sh
